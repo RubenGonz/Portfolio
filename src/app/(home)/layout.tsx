515575a976d20425b459909303dbac23e019700e
RubenGonz <ruben.gonzalez.rodriguez00@gmail.com>
2025-10-26 16:30:08 +0000

    Fix: Wrap layout in a div for consistent styling

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,11 @@
 import { Footer, Header } from "@/components";
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
-  return <>
+  return <div className="bg-surface">
     <Header />
 
     {children}
 
     <Footer />
-  </>
+  </div>
 }
