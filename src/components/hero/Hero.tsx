ba24f7396ebe50fbf393d72023104341bdf6610a
RubenGonz <ruben.gonzalez.rodriguez00@gmail.com>
2025-10-30 14:48:52 +0000

    Add hero image and enhance Hero component layout and content

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,51 +1,46 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export const Hero = () => {
-  return <section className="min-h-screen flex justify-center items-center bg-soft-black text-light px-6
-  md:flex-row md:px-16">
-    {/* Contenedor que miestar la imagen */}
-    {/* <div
-      className="absolute top-0 right-0 w-1/2 h-full bg-[url('/bg/CodingBg.png')] bg-cover bg-center"
+  return <section className="min-h-screen flex justify-center items-center bg-soft-black text-light pl-6 md:flex-row md:pl-20">
+    {/* Bg Image */}
+    <div className="absolute top-0 right-0 w-7/10 h-full bg-[url('/bg/CodingBg.png')] opacity-10 bg-cover bg-center"
       style={{
-        maskImage: "linear-gradient(to left, black 70%, transparent 100%)",
-        WebkitMaskImage: "linear-gradient(to left, black 70%, transparent 100%)",
+        maskImage: "linear-gradient(300deg, white 40%, transparent 60%)",
+        WebkitMaskImage: "linear-gradient(300deg, white 40%, transparent 60%)",
       }}
-    /> */}
+    />
 
-    {/* Texto principal */}
+    {/* Main Content */}
     <div className="flex-1 flex flex-col items-center md:items-start text-center md:text-left">
-      <h1 className="font-n27 text-brand font-bold italic text-4xl md:text-6xl mb-7">
-        Soy web <br /> developer
+      <h1 className="font-n27 text-brand font-bold italic mb-7">
+        <span className="text-4xl md:text-8xl">Soy desarrollador</span>
+        <br />
+        <span className="text-4xl md:text-9xl">Web</span>
       </h1>
-      <p className="text-transparent bg-clip-text bg-linear-to-r from-red-500 to-red-200 font-semibold text-xl">
-        Este es un texto <br /> con gradiente.
+
+      <p className="text-xl mb-7">
+        Doy forma a tus ideas a través del código. <br />
+        Construyendo. Aprendiendo. Evolucionando.<br />
+        Siempre en busca de equilibrio entre lo funcional y lo simple.
       </p>
-      <p className="text-md mb-7">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe quae explicabo incidunt, dolorum modi laboriosam laudantium cum. Esse numquam, expedita suscipit amet quam assumenda repellat dolor fugit blanditiis a praesentium!
-      </p>
+
       <div className="flex font-n27">
-        <Link
-          href="#sobre-mi"
-          className="bg-blue-600 text-light px-6 py-3 rounded text-lg
-          rounded-s-full"
-        >
+        <Link href="#sobre-mi" className="bg-blue-600 text-light px-6 py-3 rounded text-lg rounded-s-full">
           MÁS SOBRE MÍ
         </Link>
-        <div className="bg-amber-300 rounded-e-full">s</div>
-        <Link
-          href="#github"
-          className="bg-blue-600 text-light px-6 py-3 text-lg
-          rounded-full"
-        >
-        </Link>
+
+        <Link href="https://github.com/RubenGonz" target="_blank" rel="noopener noreferrer"
+          className="bg-blue-600 text-light px-6 py-3 text-lg rounded-full">Github</Link>
+        <Link href="https://www.linkedin.com/in/ruben-gonz/" target="_blank" rel="noopener noreferrer"
+          className="bg-blue-600 text-light px-6 py-3 text-lg rounded-full">Linkedin</Link>
       </div>
     </div>
 
-    {/* Placeholder para la imagen */}
-    <div className="flex-1 hidden md:flex justify-center z-1">
-      <div className="w-64 h-64 bg-gray-700 rounded-lg flex items-center justify-center">
-        {/* Aquí podrías meter <Image /> de next/image más adelante */}
-        <span className="text-gray-300">Imagen</span>
+    {/* Hero Image */}
+    <div className="hidden md:flex flex-1 h-screen justify-end items-end">
+      <div className="relative w-full h-[90%]">
+        <Image alt="Hero" src="/images/hero.png" fill className="object-contain object-bottom-right" />
       </div>
     </div>
   </section>
